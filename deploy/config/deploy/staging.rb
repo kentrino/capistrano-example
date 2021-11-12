45993a3d4a455b4f4e0d802002f39d2993b4741f
kentrino <kentrino@h-kento.jp>
2021-11-12 17:43:28 +0900

    cap install

---
+++ deploy/config/deploy/staging.rb
@@ -0,0 +1,10 @@
+# role :web, %w{root@localhost}
+
+# https://stackoverflow.com/questions/25564935/capistrano-3-set-ssh-port-but-22-is-still-used
+server "localhost", user: "root", password: "test", roles: %w{web}, port: 3022
+
+# set :ssh_options, {
+#   port: 3022,
+#   forward_agent: true,
+#   keys: ['~/.ssh/id_rsa']
+# }
